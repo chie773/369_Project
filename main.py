5adfb80553d7eee67e3eb6269fe8d52ed81788ce
chie773 <ceburuoh@gmail.com>
2026-04-16 03:03:23 -0400

    Torture

--- main.py
+++ main.py
@@ -6,26 +6,30 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 # https://sites.google.com/chromium.org/driver/
 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
+wait = WebDriverWait(driver, 10)
 
 driver.get("https://app.grabdocs.com/")
+time.sleep(5)
 
-time.sleep(10)
+original_window = driver.current_window_handle
+assert len(driver.window_handles) == 1
 
 input_element = driver.find_element(By.ID, "username")
 input_element.send_keys("chieburuoh@gmail.com")
 
-time.sleep(2)
-
 input_element = driver.find_element(By.ID, "password")
 input_element.send_keys("Chibueze123!")
 
-time.sleep(2)
+time.sleep(1)
 
 element = driver.find_elements(By.TAG_NAME, "button")
 for e in element:
@@ -38,6 +42,89 @@ time.sleep(20)
 
 
 driver.find_element(By.CSS_SELECTOR, '[title="Reach"]').click()
+
+time.sleep(5)
+
+elements = driver.find_elements(By.TAG_NAME, "button")
+
+for e in elements:
+    if e.text == "Create Meeting":
+        e.click()
+        break
+
+time.sleep(5)
+
+input_element = driver.find_elements(By.TAG_NAME, "input")
+for i in input_element:
+    if i.get_attribute("placeholder") == "Enter meeting name":
+        i.send_keys("Test Meeting")
+
+time.sleep(5)
+
+elements = driver.find_element(By.CSS_SELECTOR, '[class="bg-white dark:bg-gray-800 rounded-lg shadow-xl max-w-sm w-full mx-4 border border-gray-200 dark:border-gray-700"]')
+print(elements.tag_name)
+s_element = elements.find_elements(By.TAG_NAME, "button")
+
+
+for e in s_element:
+    if e.text == "Create Meeting":
+        e.click()
+        break
+
+time.sleep(10)
+
+
+s_element = driver.find_elements(By.TAG_NAME, "button")
+
+for e in s_element:
+    if e.get_attribute("title") == "Join meeting":
+        e.click()
+        break
+
+time.sleep(5)
+
+
+elements = driver.find_element(By.CSS_SELECTOR, '[class="w-full text-blue-600 dark:text-blue-400 hover:underline font-medium py-2"]').click()
+
+
+# for e in s_element:
+#     if e.text == "Continue without microphone and camera":
+#         e.click()
+#         break
+
+
+time.sleep(10)
+
+
+wait.until(EC.number_of_windows_to_be(2))
+for window_handle in driver.window_handles:
+        if window_handle != original_window:
+            driver.switch_to.window(window_handle)
+            break
+
+time.sleep(10)
+
+input_element = driver.find_elements(By.TAG_NAME, "input")
+for i in input_element:
+    if i.get_attribute("placeholder") == "Enter name":
+        i.send_keys("Chibueze Eburuoh")
+        break
+
+time.sleep(10)
+
+
+s_element = driver.find_elements(By.TAG_NAME, "button")
+
+
+for e in s_element:
+    if e.text == "Join Now":
+        e.click()
+        break
+
+time.sleep(20)
+
+driver.quit()
+
 
 
 # input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
